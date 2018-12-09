ff9a20ba5e4b29fab91dc6ddba63b846ff7329bc
yuki komatsu <yuki.k.lion@gmail.com>
2018-12-09 20:34:37 +0900

    add: vuejs

---
+++ cabity_calc/cavity.js
@@ -0,0 +1,14 @@
+// window.onload = function(){
+//   console.log("onload");
+// };
+
+//test
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+});
+app.message = 'I have change the messsage data!';
+
+//end of test
